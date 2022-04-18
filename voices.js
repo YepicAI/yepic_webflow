215f4b3a71490c1994bee889f204b3fb1db16cd5
flachdaniel <email@flachdaniel.com>
2022-04-18 15:44:32 +0200

    fix

--- voices.js
+++ voices.js
@@ -1,10 +1,12 @@
 var audioElement = document.createElement('audio');
 var audioState = "stopped"
+var actualAudio = ""
 
 $(".form-tab-voice-wrap").on("click",".form-voice-sample", function () {    
       if (audioState == "stopped") {
             console.log("stopped state");
-            $(this).children("div").removeClass("play").addClass("pause");
+            actualAudio = $(this).children("div");
+            actualAudio.removeClass("play").addClass("pause");
             var audioSrc = $(this).attr("data-src");
             audioElement.setAttribute('src', audioSrc);
             audioElement.play();
@@ -17,7 +19,7 @@ $(".form-tab-voice-wrap").on("click",".form-voice-sample", function () {
             });
       } else {
             console.log("playing state");
-            $(this).children("div").removeClass("pause").addClass("play");
+            actualAudio.removeClass("pause").addClass("play");
             audioElement.pause();
             audioElement.currentTime = 0;
             audioState = "stopped"
