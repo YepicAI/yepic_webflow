d178bb52efb76ac17706f34ce42433b46d879177
flachdaniel <email@flachdaniel.com>
2022-04-18 16:07:35 +0200

    remove console logs

--- voices.js
+++ voices.js
@@ -14,20 +14,17 @@ $(".form-tab-voice-wrap").on("click",".form-voice-sample", function () {
       }
       lastVoice = $(this).attr("data-src");
       if (audioState == "stopped") {        
-            console.log("stopped state");
             actualAudio = $(this).children("div");
             actualAudio.removeClass("play").addClass("pause");
             audioElement.setAttribute('src', lastVoice);
             audioElement.play();
             audioState = "playing";
             audioElement.addEventListener('ended', function() {
-                  console.log("ended");
                   actualAudio.removeClass("pause").addClass("play");
                   audioElement.currentTime = 0;
                   audioState = "stopped"
             });
       } else {
-            console.log("playing state");
             $(this).children("div").removeClass("pause").addClass("play");
             audioElement.pause();
             audioElement.currentTime = 0;
