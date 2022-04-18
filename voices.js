d517ef72d9ff2c3bb797fc26965ae39829e6267f
flachdaniel <email@flachdaniel.com>
2022-04-18 15:12:34 +0200

    add play

--- voices.js
+++ voices.js
@@ -1,13 +1,14 @@
 
 
 var audioElement = document.createElement('audio');
+var pauseSymbol = "url(https://assets-global.website-files.com/603a1632f3d4a6c0f66872b9/6080782b0edbdbe9c0e96234_pause.svg)";
+var playSymbol = "url(https://assets-global.website-files.com/603a1632f3d4a6c0f66872b9/60815d642f83b515282a9b1b_play.svg)";
 
 $(".form-tab-voice-wrap").on("click",".form-voice-sample", function () {
+      
       console.log("asd");
-
-      const formVoiceIcon = $(this).children("div");
+      const formVoiceIcon = $(this).children("div").removeClass("play").addClass("pause");
       var audioSrc = $(this).attr("data-src");
-      console.log(audioSrc);
-      var pauseSymbol = "url(https://assets-global.website-files.com/603a1632f3d4a6c0f66872b9/6080782b0edbdbe9c0e96234_pause.svg)";
-      var playSymbol = "url(https://assets-global.website-files.com/603a1632f3d4a6c0f66872b9/60815d642f83b515282a9b1b_play.svg)";
+      audioElement.setAttribute('src', audioSrc);
+      audioElement.play()   
 });
