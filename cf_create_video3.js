89c344aa2f3d2bd24a29f28500e9fc12d21906cc
ac-yai <95138604+ac-yai@users.noreply.github.com>
2022-05-12 21:05:24 +0100

    update

--- cf_create_video3.js
+++ cf_create_video3.js
@@ -1,33 +1,33 @@
 var audio_preview_state = '';
 
 var video_request_model = {
-    actor: 'Alex', // submit from create video page
-    voice: '', // submit from create video page 
-    voice_api_provider: '', // submit from create video page
-    voice_provider: '', // submit from create video page
-    script: '', // submit from create video page
-    custom_audio_file: '', // if user submitted a custom audio upload
-    audio_file: '', // backend generated - for gallery
-    current_video_most_recent: '', // backend generated - for gallery
-    audio_fea_link: '', // backend only
-    avatar_type: 'full-body', // submit from create video page
-    background_url: 'https://storage.googleapis.com/yepicai-backend.appspot.com/regularBackgrounds/office-background-FHD.png', // submit from create video page
-    script_approval: '', // backend generated - for gallery
-    create_video: '', // backend generated - for gallery
-    watermarked: '', // backend generated - for gallery
-    avatar_size: '', // submit from create video page
-    avatar_position: 'centre', // submit from create video page
-    avatar_circle_background: '', // submit from create video page
-    avatar_circle_background_rim: '', // submit from create video page
-    avatar_size_circle: '', // submit from create video page
-    vm_status: '', // backend generated for debugging only
-    memberstack_id: '', // taken from JWT token
-    video_name: '', // submit from create video page
-    unique_webpage: '', // backend generated - for gallery
-    date_created: '', // backend generated - for gallery
-    date_modified: '', // not used by frontend
-    download_url: '', // backend generated - for gallery
-    preview_image_url: '', // not currently used e.g. 'https://assets-global.website-files.com/603a1632f3d4a6c0f66872b9/6082b99fff1618b81cc1b433_khamal-p-500.png'
+    actor                       : 'Alex',                                                                                                      // submit from create video page
+    voice                       : '',                                                                                                          // submit from create video page 
+    voice_api_provider          : '',                                                                                                          // submit from create video page
+    voice_provider              : '',                                                                                                          // submit from create video page
+    script                      : '',                                                                                                          // submit from create video page
+    custom_audio_file           : '',                                                                                                          // if user submitted a custom audio upload
+    audio_file                  : '',                                                                                                          // backend generated - for gallery
+    current_video_most_recent   : '',                                                                                                          // backend generated - for gallery
+    audio_fea_link              : '',                                                                                                          // backend only
+    avatar_type                 : 'full-body',                                                                                                 // submit from create video page
+    background_url              : 'https://storage.googleapis.com/yepicai-backend.appspot.com/regularBackgrounds/office-background-FHD.png',   // submit from create video page
+    script_approval             : '',                                                                                                          // backend generated - for gallery
+    create_video                : '',                                                                                                          // backend generated - for gallery
+    watermarked                 : '',                                                                                                          // backend generated - for gallery
+    avatar_size                 : '',                                                                                                          // submit from create video page
+    avatar_position             : 'centre',                                                                                                    // submit from create video page
+    avatar_circle_background    : '',                                                                                                          // submit from create video page
+    avatar_circle_background_rim: '',                                                                                                          // submit from create video page
+    avatar_size_circle          : '',                                                                                                          // submit from create video page
+    vm_status                   : '',                                                                                                          // backend generated for debugging only
+    memberstack_id              : '',                                                                                                          // taken from JWT token - ignored on backend
+    video_name                  : '',                                                                                                          // submit from create video page
+    unique_webpage              : '',                                                                                                          // backend generated - for gallery
+    date_created                : '',                                                                                                          // backend generated - for gallery
+    date_modified               : '',                                                                                                          // not used by frontend
+    download_url                : '',                                                                                                          // backend generated - for gallery
+    preview_image_url           : '',                                                                                                          // not currently used e.g. 'https://assets-global.website-files.com/603a1632f3d4a6c0f66872b9/6082b99fff1618b81cc1b433_khamal-p-500.png'
 };
 
 async function get_audio_preview(voice, voice_provider, voice_api_provider) {
@@ -55,7 +55,7 @@ async function submit_video_request() {
         method: 'POST',
         body: JSON.stringify(video_request_model),
         headers: {
-            "Content-Type": "application/json",
+            "Content-Type" : "application/json",
             "Authorization": "Bearer " + MemberStack.getToken(),
         },
     });
@@ -95,18 +95,18 @@ window.addEventListener("load", async (e) => {
         actor_select_buttons[i].addEventListener("click", async (e) => {
             t = e.currentTarget;
             video_request_model.actor = t.getAttribute('data-actor');
-        }); // ,true});
+        }, true);
     }
     
     var voice_select_buttons = document.querySelectorAll('div[data-voice][data-voice-provider][data-voice-api-provider].form-voice');
 
     for (let i = 0; i < voice_select_buttons.length; i++) {
         voice_select_buttons[i].addEventListener("click", async (e) => {
-            t = e.currentTarget;
-            video_request_model.voice = t.getAttribute('data-voice');
-            video_request_model.voice_provider = t.getAttribute('data-voice-provider');
+            t                                      = e.currentTarget;
+            video_request_model.voice              = t.getAttribute('data-voice');
+            video_request_model.voice_provider     = t.getAttribute('data-voice-provider');
             video_request_model.voice_api_provider = t.getAttribute('data-voice-api-provider');
-        }); // ,true});
+        }, true);
     }
 
 });
