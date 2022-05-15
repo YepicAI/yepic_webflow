074e1329b3db41a3f62196cfe2ce6231fd0e9929
ac-yai <95138604+ac-yai@users.noreply.github.com>
2022-05-15 15:20:17 +0100

    update

--- create_video_fix.js
+++ create_video_fix.js
@@ -80,111 +80,192 @@ console.log("------------------------version: 01 ----------------------------");
 
 
 // ------------------------------------------------- SELECT ACTOR POSITION AND TYPE -------------------------------------------------
-
-//----------- Functions (type and positions) -----------
-function selectActorPositionAndType(actorPosition, actorType, imageClassName) {
-  console.log("------------ position: " + $(actorPosition).attr("data-position") + " and type: " + actorType);
-  position = $(actorPosition).attr("data-position");
-  $(".actor-pos").css({ borderColor: "transparent" });
-  $($(actorPosition)).css(borderCss);
-  if (actorType == "full-body") {
-    actorTypePositionSelection.fullBody = position;
-    actorTypePositionSelection.classNameFullBody = actorPosition;
-    actorTypePositionSelection.classNameFullBodyImage = imageClassName;
-    $(".preview-img-wrap").removeClass("preview-img-left preview-img-mid preview-img-right");
-    $(".preview-img-wrap").addClass(imageClassName);
-  };
-  if (actorType == "circle") {
-    actorTypePositionSelection.circle = position;
-    actorTypePositionSelection.classNameCircle = actorPosition;
-    actorTypePositionSelection.classNameCircleImage = imageClassName;
-    $(".preview-circle-img-wrap").removeClass("t1 t2 t3 m1 m2 m3 b1 b2 b3");
-    $(".preview-circle-img-wrap").addClass(imageClassName);
-  }
-  video_request_model.avatar_position = position;
-  video_request_model.avatar_type = actorType;
-};
-
-async function changeCircleBackground(colorObject) {
-  hexCode = colorObject.attr("data-hexcode");
-  video_request_model.avatar_circle_background = "#" + hexCode;
-  console.log(video_request_model.avatar_circle_background);
-
-  class_list = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8'];
-
-  for (let i = 0; i < class_list.length; i++) {
-    if (colorObject.hasClass(class_list[i])) {
-      backgroundColorClass = class_list[i];
-      //break;
+  
+  //----------- Functions (type and positions) -----------
+  function selectActorPositionAndType(actorPosition, actorType, imageClassName) {
+    console.log("------------ position: " + $(actorPosition).attr("data-position") + " and type: " + actorType);
+    position = $(actorPosition).attr("data-position");
+    $(".actor-pos").css({ borderColor: "transparent" });
+    $($(actorPosition)).css(borderCss);
+    if (actorType == "full-body") {
+      actorTypePositionSelection.fullBody = position;
+      actorTypePositionSelection.classNameFullBody = actorPosition;
+      actorTypePositionSelection.classNameFullBodyImage = imageClassName;
+      $(".preview-img-wrap").removeClass("preview-img-left preview-img-mid preview-img-right");
+      $(".preview-img-wrap").addClass(imageClassName);
+    };
+    if (actorType == "circle") {
+      actorTypePositionSelection.circle = position;
+      actorTypePositionSelection.classNameCircle = actorPosition;
+      actorTypePositionSelection.classNameCircleImage = imageClassName;
+      $(".preview-circle-img-wrap").removeClass("t1 t2 t3 m1 m2 m3 b1 b2 b3");
+      $(".preview-circle-img-wrap").addClass(imageClassName);
     }
-  }
-
-  $(".form-tab-circle-bg").css({ borderColor: "transparent" });
-  colorObject.css(borderCss);
-  $("#previewCircImg").removeClass("c1 c2 c3 c4 c5 c6 c7 c8");
-  $("#previewCircImg").addClass(backgroundColorClass);
-};
-
-//----------- FULL-BODY/CIRCLE Tab selection -----------
-$("#tab-title-full").click(function () {
-  selectActorPositionAndType(actorTypePositionSelection.classNameFullBody, "full-body", actorTypePositionSelection.classNameFullBodyImage);
-  $(".preview-img-wrap").children("img").attr("src", video_request_model.preview_image_url);
-  $(".preview-circle-img-wrap").hide();
-  $(".preview-img-wrap").show();
-  console.log("1");
-  video_request_model.avatar_type = "full-body";
-  video_request_model.avatar_position = actorTypePositionSelection.fullBody;
-});
-$("#tab-title-circle").click(function () {
-  selectActorPositionAndType(actorTypePositionSelection.classNameCircle, "circle", actorTypePositionSelection.classNameCircleImage);
-  $(".preview-circle-img-wrap").children().children("img").attr("src", video_request_model.preview_image_url);
-  $(".preview-img-wrap").hide();
-  $(".preview-circle-img-wrap").show();
-  video_request_model.avatar_type = "circle";
-  video_request_model.avatar_position = actorTypePositionSelection.circle;
-});
-
-// set avatar_position click events
-async function set_avatar_position_click_events()
-{
-  var items = {
-      ".actor-pos-left"            : ["full-body", "preview-img-left"],
-      ".actor-pos-mid"             : ["full-body", "preview-img-mid"],
-      ".actor-pos-right"           : ["full-body", "preview-img-right"],
-      ".actor-pos-circle-topleft"  : ["circle", "t1"],
-      ".actor-pos-circle-topcentre": ["circle", "t2"],
-      ".actor-pos-circle-topright" : ["circle", "t3"],
-      ".actor-pos-circle-midleft"  : ["circle", "m1"],
-      ".actor-pos-circle-midcentre": ["circle", "m2"],
-      ".actor-pos-circle-midright" : ["circle", "m3"],
-      ".actor-pos-circle-botleft"  : ["circle", "b1"],
-      ".actor-pos-circle-botcentre": ["circle", "b2"],
-      ".actor-pos-circle-botright" : ["circle", "b3"],
+    fV.position = position;
+    fV.actorPositionType = actorType;
   };
-
-  for (const [key, value] of Object.entries(items)) {
-    const i_key = key;
-    const i_value = value;
-
-    let element = document.querySelector(key);
-
-    element.addEventListener("click", async (e) => {
-      selectActorPositionAndType(i_key, i_value[0], i_value[1]);
-    });    
-  }
-}
-
-//----------- CIRCLE BACKGROUND selection -----------
-async function set_circle_background_click_events() {
-  var element = document.querySelector("#circle-background-select");
-
-  element.addEventListener("click", async (e) => {
-    await changeCircleBackground($(this));
+  
+  function changeCircleBackground(colorObject) {
+    hexCode = colorObject.attr("data-hexcode");
+    fV.circleBackgroundColor = "#" + hexCode;
+    console.log(fV.circleBackgroundColor);
+  
+    if (colorObject.hasClass('c1')) {
+      backgroundColorClass = 'c1'
+    }
+    if (colorObject.hasClass('c2')) {
+      backgroundColorClass = 'c2'
+    }
+    if (colorObject.hasClass('c3')) {
+      backgroundColorClass = 'c3'
+    }
+    if (colorObject.hasClass('c4')) {
+      backgroundColorClass = 'c4'
+    }
+    if (colorObject.hasClass('c5')) {
+      backgroundColorClass = 'c5'
+    }
+    if (colorObject.hasClass('c6')) {
+      backgroundColorClass = 'c6'
+    }
+    if (colorObject.hasClass('c7')) {
+      backgroundColorClass = 'c7'
+    }
+    if (colorObject.hasClass('c8')) {
+      backgroundColorClass = 'c8'
+    }
+    $(".form-tab-circle-bg").css({ borderColor: "transparent" });
+    colorObject.css(borderCss);
+    $("#previewCircImg").removeClass("c1 c2 c3 c4 c5 c6 c7 c8");
+    $("#previewCircImg").addClass(backgroundColorClass);
+  };
+  
+  //----------- FULL-BODY/CIRCLE Tab selection -----------
+  $("#tab-title-full").click(function () {
+    selectActorPositionAndType(actorTypePositionSelection.classNameFullBody, "full-body", actorTypePositionSelection.classNameFullBodyImage);
+    $(".preview-img-wrap").children("img").attr("src", fV.previewImgSrc);
+    $(".preview-circle-img-wrap").hide();
+    $(".preview-img-wrap").show();
+    console.log("1");
+    fV.actorType = "full-body";
+    fV.position = actorTypePositionSelection.fullBody;
   });
-}
+  $("#tab-title-circle").click(function () {
+    selectActorPositionAndType(actorTypePositionSelection.classNameCircle, "circle", actorTypePositionSelection.classNameCircleImage);
+    $(".preview-circle-img-wrap").children().children("img").attr("src", fV.previewImgSrc);
+    $(".preview-img-wrap").hide();
+    $(".preview-circle-img-wrap").show();
+    fV.actorType = "circle";
+    fV.position = actorTypePositionSelection.circle;
+  });
+  
+  //----------- FULL-BODY selection -----------
+  $(".actor-pos-left").click(function () {
+    selectActorPositionAndType(".actor-pos-left", "full-body", "preview-img-left");
+  });
+  $(".actor-pos-mid").click(function () {
+    selectActorPositionAndType(".actor-pos-mid", "full-body", "preview-img-mid");
+  });
+  $(".actor-pos-right").click(function () {
+    selectActorPositionAndType(".actor-pos-right", "full-body", "preview-img-right");
+  });
+  
+  //----------- CIRCLE-BODY selection -----------
+  $(".actor-pos-circle-topleft").click(function () {
+    selectActorPositionAndType(".actor-pos-circle-topleft", "circle", "t1");
+  });
+  $(".actor-pos-circle-topcentre").click(function () {
+    selectActorPositionAndType(".actor-pos-circle-topcentre", "circle", "t2");
+  });
+  $(".actor-pos-circle-topright").click(function () {
+    selectActorPositionAndType(".actor-pos-circle-topright", "circle", "t3");
+  });
+  $(".actor-pos-circle-midleft").click(function () {
+    selectActorPositionAndType(".actor-pos-circle-midleft", "circle", "m1");
+  });
+  $(".actor-pos-circle-midcentre").click(function () {
+    selectActorPositionAndType(".actor-pos-circle-midcentre", "circle", "m2");
+  });
+  $(".actor-pos-circle-midright").click(function () {
+    selectActorPositionAndType(".actor-pos-circle-midright", "circle", "m3");
+  });
+  $(".actor-pos-circle-botleft").click(function () {
+    selectActorPositionAndType(".actor-pos-circle-botleft", "circle", "b1");
+  });
+  $(".actor-pos-circle-botcentre").click(function () {
+    selectActorPositionAndType(".actor-pos-circle-botcentre", "circle", "b2");
+  });
+  $(".actor-pos-circle-botright").click(function () {
+    selectActorPositionAndType(".actor-pos-circle-botright", "circle", "b3");
+  });
+  
+  //----------- CIRCLE BACKGROUND selection -----------
+  
+  $(".form-circ-colours").on("click", "#circle-background-select", function () {
+    changeCircleBackground($(this));
+  });
+  
 
-(async () => set_avatar_position_click_events())();
-(async () => set_circle_background_click_events())();
+
+// function changeCircleBackground(colorObject) {
+//   hexCode = colorObject.attr("data-hexcode");
+//   video_request_model.avatar_circle_background = "#" + hexCode;
+//   console.log(video_request_model.avatar_circle_background);
+
+//   class_list = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8'];
+
+//   for (let i = 0; i < class_list.length; i++) {
+//     if (colorObject.hasClass(class_list[i])) {
+//       backgroundColorClass = class_list[i];
+//       //break;
+//     }
+//   }
+
+//   $(".form-tab-circle-bg").css({ borderColor: "transparent" });
+//   colorObject.css(borderCss);
+//   $("#previewCircImg").removeClass("c1 c2 c3 c4 c5 c6 c7 c8");
+//   $("#previewCircImg").addClass(backgroundColorClass);
+// };
+// set avatar_position click events
+// async function set_avatar_position_click_events()
+// {
+//   var items = {
+//       ".actor-pos-left"            : ["full-body", "preview-img-left"],
+//       ".actor-pos-mid"             : ["full-body", "preview-img-mid"],
+//       ".actor-pos-right"           : ["full-body", "preview-img-right"],
+//       ".actor-pos-circle-topleft"  : ["circle", "t1"],
+//       ".actor-pos-circle-topcentre": ["circle", "t2"],
+//       ".actor-pos-circle-topright" : ["circle", "t3"],
+//       ".actor-pos-circle-midleft"  : ["circle", "m1"],
+//       ".actor-pos-circle-midcentre": ["circle", "m2"],
+//       ".actor-pos-circle-midright" : ["circle", "m3"],
+//       ".actor-pos-circle-botleft"  : ["circle", "b1"],
+//       ".actor-pos-circle-botcentre": ["circle", "b2"],
+//       ".actor-pos-circle-botright" : ["circle", "b3"],
+//   };
+
+//   for (const [key, value] of Object.entries(items)) {
+//     const i_key = key;
+//     const i_value = value;
+
+//     let element = document.querySelector(key);
+
+//     element.addEventListener("click", async (e) => {
+//       selectActorPositionAndType(i_key, i_value[0], i_value[1]);
+//     });    
+//   }
+// }
+
+// async function set_circle_background_click_events() {
+//   var element = document.querySelector("#circle-background-select");
+
+//   element.addEventListener("click", async (e) => {
+//     await changeCircleBackground($(this));
+//   });
+// }
+
+// (async () => set_avatar_position_click_events())();
+// (async () => set_circle_background_click_events())();
 
 // ------------------------------------------------- SELECT VOICE AND ACTOR -------------------------------------------------
 
