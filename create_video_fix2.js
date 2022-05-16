8ee208db7675bfea78ce0c901d8713a1a86b5ffc
ac-yai <95138604+ac-yai@users.noreply.github.com>
2022-05-16 19:45:06 +0100

    update

--- create_video_fix2.js
+++ create_video_fix2.js
@@ -53,7 +53,7 @@ var video_request_model = {
   date_created: '', // backend generated - for gallery
   date_modified: '', // not used by frontend
   download_url: '', // backend generated - for gallery
-  preview_image_url: '', // not currently used e.g. 'https://assets-global.website-files.com/603a1632f3d4a6c0f66872b9/6082b99fff1618b81cc1b433_khamal-p-500.png'
+  preview_image_url: 'https://assets-global.website-files.com/603a1632f3d4a6c0f66872b9/6082b99fff1618b81cc1b433_khamal-p-500.png'
 };
 
 var actorTypePositionSelection = {
